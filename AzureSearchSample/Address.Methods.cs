c044eabc1d8809e51f098be7d4a77b883788bb25
Sandeep.S.Kumar7 <Sandeep.S.Kumar7@shell.com>
2020-05-07 04:13:54 +0530

    Azure search sample

---
+++ AzureSearchSample/Address.Methods.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace AzureSearchSample
+{
+    public partial class Address
+    {
+        public override string ToString() =>
+            IsEmpty ?
+                string.Empty :
+                $"{StreetAddress}\n{City}, {StateProvince} {PostalCode}\n{Country}";
+
+        [JsonIgnore]
+        public bool IsEmpty => String.IsNullOrEmpty(StreetAddress) &&
+                               String.IsNullOrEmpty(City) &&
+                               String.IsNullOrEmpty(StateProvince) &&
+                               String.IsNullOrEmpty(PostalCode) &&
+                               String.IsNullOrEmpty(Country);
+    }
+}
---
+++ AzureSearchSample/Office.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Azure.Search;
+using Microsoft.Azure.Search.Models;
+using Newtonsoft.Json;
+
+namespace AzureSearchSample
+{
+    public partial class Office
+    {
+        [System.ComponentModel.DataAnnotations.Key]
+        [IsFilterable]
+        public string OfficeId { get; set; }
+
+        [IsSearchable, IsSortable]
+        public string OfficeName { get; set; }
+
+        [IsSearchable]
+        [Analyzer(AnalyzerName.AsString.EnMicrosoft)]
+        public string Description { get; set; }
+
+        [IsSearchable]
+        [Analyzer(AnalyzerName.AsString.FrLucene)]
+        [JsonProperty("Description_fr")]
+        public string DescriptionFr { get; set; }
+
+        [IsSearchable, IsFilterable, IsSortable, IsFacetable]
+        public string Category { get; set; }
+
+        [IsSearchable, IsFilterable, IsFacetable]
+        public string[] Tags { get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public bool? ParkingIncluded { get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public DateTimeOffset? LastRenovationDate { get; set; }
+
+        [IsFilterable, IsSortable, IsFacetable]
+        public double? Rating { get; set; }
+
+        public Address Address { get; set; }
+    }
+}
